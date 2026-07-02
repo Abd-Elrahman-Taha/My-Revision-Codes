0c4c93cefda250b04b812a2096f18055ac21d57f
Abdelrahman Taha <b0ody66taha@gmail.com>
2026-07-02 18:56:42 +0300

    fixing the navbar

--- React/Redux/src/pages/home.tsx
+++ React/Redux/src/pages/home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
       <section className="mx-auto flex min-h-[90vh] max-w-7xl flex-col items-center justify-between gap-12 px-6 py-20 lg:flex-row">
 
         <div className="max-w-2xl">
-          <span className="rounded-full bg-[#72470a] px-4 py-2 text-sm font-semibold text-white">
+          <span className="welcome rounded-full bg-[#72470a] px-4 py-2 text-sm font-semibold text-white">
             Welcome to ShopX
           </span>
 
@@ -72,7 +72,7 @@ const Home = () => {
 
           <div className="grid gap-8 md:grid-cols-2 xl:grid-cols-4">
 
-            <div className="rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
+            <div className=" image rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
               <FaShoppingBag
                 className="mb-6 text-[#8A735A]"
                 size={40}
@@ -87,7 +87,7 @@ const Home = () => {
               </p>
             </div>
 
-            <div className="rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
+            <div className=" image rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
               <FaShippingFast
                 className="mb-6 text-green-600"
                 size={40}
@@ -102,7 +102,7 @@ const Home = () => {
               </p>
             </div>
 
-            <div className="rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
+            <div className=" image rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
               <FaShieldAlt
                 className="mb-6 text-red-500"
                 size={40}
@@ -117,7 +117,7 @@ const Home = () => {
               </p>
             </div>
 
-            <div className="rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
+            <div className=" image rounded-3xl bg-[#F8F5F0] p-8 shadow-sm transition hover:-translate-y-2 hover:shadow-xl">
               <FaHeadset
                 className="mb-6 text-purple-600"
                 size={40}
